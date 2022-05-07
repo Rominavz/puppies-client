dd964dbfeecba07b46742b95512ba9f8f07f1f85
Rominavz <vz_romina@hotmail.com>
2022-05-07 09:06:55 -0300

    first cmt

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { store } from '../src/store/index';
+import dotenv from 'dotenv';
+import axios from 'axios';
 dotenv.config();
 
 axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
